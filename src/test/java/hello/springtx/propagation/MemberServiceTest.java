f4ba4750ce44999835805cfda9fe0a84e5bb4667
Kidding Just <rlarkdlr@gmail.com>
2022-06-13 23:05:31 +0900

    Transaction Propagation Uses

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -33,6 +34,24 @@ class MemberServiceTest {
         //JUNIT걸 쓰면 assertTrue 라는 게 있다.
         //then: 모든 데이터가 정상 저장된다.
         Assertions.assertTrue(memberRepository.find(username).isPresent());
-    }
+        Assertions.assertTrue(logRepository.find(username).isPresent());
 
+    }
+    /**
+     * memberService    @Transactional : OFF
+     * memberRepository @Transactional : ON
+     * logRepository    @Transactional : ON Exception
+     */
+    @Test
+    void outerTxOff_fail(){
+        //given
+        String username = "로그예외 outerTxOff_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then: Member는 저장이 되지만 Log는 롤백이 된다.
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
 }
